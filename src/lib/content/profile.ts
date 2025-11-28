eeabf1d122472b274749c5afc2a64d3a39858d20
Aagam2020 <63091206+Aagam2020@users.noreply.github.com>
2025-11-28 07:53:44 -0500

    Update profile.ts

--- src/lib/content/profile.ts
+++ src/lib/content/profile.ts
@@ -1,7 +1,6 @@
 const profile = {
   name: "Aagam Shah",
   email: "aagamshah2020@gmail.com",
-  x: "https://x.com/2Ljt2kwit",
   linkedin: "https://www.linkedin.com/in/aagamshah2020/",
   github: "https://github.com/Aagam2020",
   substack: "https://aagam.substack.com/?utm_source=global-search",
